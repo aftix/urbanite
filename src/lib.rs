eb0ab004b7d74dc8ddd5035445e5dda5b9bbdbad
Wyatt Campbell <wyatt.campbell@cirrus.com>
2022-08-26 14:37:19 -0500

    render cube and point light

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use heron::prelude::*;
 use iyes_loopless::prelude::*;
 
 mod mainmenu;
@@ -20,16 +21,37 @@ struct UiRoot;
 impl Plugin for Urbanite {
     fn build(&self, app: &mut App) {
         app.add_startup_system(setup)
+            .add_plugin(PhysicsPlugin::default())
             .add_loopless_state(GameState::MainMenu)
             .add_plugin(mainmenu::MainMenu);
     }
 }
 
-fn setup(mut commands: Commands, _asst_server: Res<AssetServer>) {
-    commands.spawn_bundle(Camera3dBundle::default());
-    let mut camera2d = Camera2dBundle::default();
-    camera2d.camera.priority = 2;
-    commands.spawn_bundle(camera2d);
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    _asst_server: Res<AssetServer>,
+) {
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
     commands
         .spawn_bundle(NodeBundle {
             style: Style {
