def6246c74938480680cdac29e6544748f40a18f
Wyatt Campbell <wyatt.campbell@cirrus.com>
2022-08-26 16:30:22 -0500

    Menu selection

--- src/lib.rs
+++ src/lib.rs
@@ -27,27 +27,7 @@ impl Plugin for Urbanite {
     }
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    _asst_server: Res<AssetServer>,
-) {
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
-    commands.spawn_bundle(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    });
+fn setup(mut commands: Commands, _asst_server: Res<AssetServer>) {
     commands.spawn_bundle(Camera3dBundle {
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
--- src/mainmenu.rs
+++ src/mainmenu.rs
@@ -6,26 +6,106 @@ use iyes_loopless::prelude::*;
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Component)]
 struct MainMenuUi;
 
+// Marker component for MainMenu selected item
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Component)]
+struct Selected;
+
+// Components for navigating the menu
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Component)]
+struct Next(Entity);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Component)]
+struct Previous(Entity);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct ChangeEvent(Entity);
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub(crate) struct MainMenu;
 
 impl Plugin for MainMenu {
     fn build(&self, app: &mut App) {
         app.add_enter_system(GameState::MainMenu, menu_startup)
+            .add_event::<ChangeEvent>()
             .add_system_set(
                 ConditionSet::new()
                     .run_in_state(GameState::MainMenu)
                     .with_system(bevy::window::close_on_esc)
+                    .with_system(change_color)
+                    .with_system(change_color_removed)
+                    .with_system(move_selection)
                     .into(),
-            );
+            )
+            .add_exit_system(GameState::MainMenu, menu_teardown);
+    }
+}
+
+fn move_selection(
+    mut commands: Commands,
+    mut keys: ResMut<Input<KeyCode>>,
+    q: Query<(Entity, &Next, &Previous), With<Selected>>,
+    mut ev_writer: EventWriter<ChangeEvent>,
+) {
+    let (entity, next, prev) = q.single();
+    if keys.just_pressed(KeyCode::Up) {
+        commands.entity(entity).remove::<Selected>();
+        commands.entity(prev.0).insert(Selected);
+        ev_writer.send(ChangeEvent(entity));
+        keys.clear();
+    } else if keys.just_pressed(KeyCode::Down) {
+        commands.entity(entity).remove::<Selected>();
+        commands.entity(next.0).insert(Selected);
+        ev_writer.send(ChangeEvent(entity));
+        keys.clear();
+    }
+}
+
+fn change_color(mut q: Query<(&mut Text, Added<Selected>)>) {
+    let (mut text, _) = q.single_mut();
+    for section in &mut text.sections {
+        section.style.color = Color::RED;
+    }
+}
+
+fn change_color_removed(
+    mut ev_moved: EventReader<ChangeEvent>,
+    mut q: Query<(Entity, &mut Text), Without<Selected>>,
+) {
+    for changed in ev_moved.iter() {
+        for (entity, mut text) in &mut q {
+            if changed.0 == entity {
+                for section in &mut text.sections {
+                    section.style.color = Color::WHITE;
+                }
+            }
+        }
     }
 }
 
 fn menu_startup(
     mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
     asst_server: Res<AssetServer>,
     q: Query<(Entity, &UiRoot)>,
 ) {
+    commands
+        .spawn_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        })
+        .insert(MainMenuUi);
+    commands
+        .spawn_bundle(PointLightBundle {
+            point_light: PointLight {
+                intensity: 1500.0,
+                shadows_enabled: true,
+                ..default()
+            },
+            transform: Transform::from_xyz(4.0, 8.0, 4.0),
+            ..default()
+        })
+        .insert(MainMenuUi);
     let (uiroot, _) = q.single();
     let text = commands
         .spawn_bundle(
@@ -51,5 +131,70 @@ fn menu_startup(
         )
         .insert(MainMenuUi)
         .id();
-    commands.entity(uiroot).push_children(&[text]);
+
+    let start = commands
+        .spawn_bundle(
+            TextBundle::from_section(
+                "Start",
+                TextStyle {
+                    color: Color::WHITE,
+                    font: asst_server.load("fonts/mechanical-font/Mechanical-g5Y5.otf"),
+                    font_size: 24.0,
+                },
+            )
+            .with_text_alignment(TextAlignment::TOP_CENTER)
+            .with_style(Style {
+                position_type: PositionType::Absolute,
+                align_self: AlignSelf::FlexEnd,
+                position: UiRect {
+                    top: Val::Px(5.0),
+                    left: Val::Px(30.0),
+                    ..default()
+                },
+                ..default()
+            }),
+        )
+        .insert(MainMenuUi)
+        .insert(Selected)
+        .id();
+
+    let quit = commands
+        .spawn_bundle(
+            TextBundle::from_section(
+                "Quit",
+                TextStyle {
+                    color: Color::WHITE,
+                    font: asst_server.load("fonts/mechanical-font/Mechanical-g5Y5.otf"),
+                    font_size: 24.0,
+                },
+            )
+            .with_text_alignment(TextAlignment::TOP_CENTER)
+            .with_style(Style {
+                position_type: PositionType::Absolute,
+                align_self: AlignSelf::FlexEnd,
+                position: UiRect {
+                    top: Val::Px(35.0),
+                    left: Val::Px(30.0),
+                    ..default()
+                },
+                ..default()
+            }),
+        )
+        .insert(MainMenuUi)
+        .insert(Next(start))
+        .insert(Previous(start))
+        .id();
+
+    commands
+        .entity(start)
+        .insert(Next(quit))
+        .insert(Previous(quit));
+
+    commands.entity(uiroot).push_children(&[text, start, quit]);
+}
+
+fn menu_teardown(mut commands: Commands, q: Query<(Entity, &MainMenuUi)>) {
+    for (entity, _) in &q {
+        commands.entity(entity).despawn_recursive();
+    }
 }
